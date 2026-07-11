5d9e97d68ef58ae15078e8a3378ff099c8825e5f
sanjaypanneer262005-stack <sanjaypanneer262005@gmail.com>
2026-07-11 22:41:09 +0530

    Update model name in gemini.config.js

--- src/config/gemini.config.js
+++ src/config/gemini.config.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-const MODEL_NAME = "gemini-2.5-flash-lite";
+const MODEL_NAME = "gemini-2.5-flash";
 
 const generateContent = async (prompt) => {
   try {
